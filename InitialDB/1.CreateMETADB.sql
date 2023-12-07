f85ae300711bf9909a81c5405afba3633e0abfc3
Huynhminhduc2k2 <hmduc20@clc.fitus.edu.vn>
2023-12-07 23:06:05 +0700

    Create db for NDS

--- InitialDB/1.CreateMETADB.sql
+++ InitialDB/1.CreateMETADB.sql
@@ -18,6 +18,7 @@ SELECT * FROM DATA_FLOW
 DELETE from DATA_FLOW
 
 --Cập nhật lại CET và LSET để nạp vào STAGE
+--STAGE
 update DATA_FLOW SET LSET = '2006-01-01 00:00:00' WHERE NAME = 'supermarket_sales_STAGE'
 update DATA_FLOW SET CET = '2006-01-01 00:00:00' WHERE NAME = 'supermarket_sales_STAGE'
 
@@ -30,9 +31,27 @@ update DATA_FLOW SET CET = '2006-01-01 00:00:00' WHERE NAME = 'product_line_STAG
 update DATA_FLOW SET LSET = '2006-01-01 00:00:00' WHERE NAME = 'city_STAGE'
 update DATA_FLOW SET CET = '2006-01-01 00:00:00' WHERE NAME = 'city_STAGE'
 
+--NDS
+update DATA_FLOW SET LSET = '2006-01-01 00:00:00' WHERE NAME = 'supermarket_sales_NDS'
+update DATA_FLOW SET CET = '2006-01-01 00:00:00' WHERE NAME = 'supermarket_sales_NDS'
+
+update DATA_FLOW SET LSET = '2006-01-01 00:00:00' WHERE NAME = 'product_NDS'
+update DATA_FLOW SET CET = '2006-01-01 00:00:00' WHERE NAME = 'product_NDS'
+
+update DATA_FLOW SET LSET = '2006-01-01 00:00:00' WHERE NAME = 'product_line_NDS'
+update DATA_FLOW SET CET = '2006-01-01 00:00:00' WHERE NAME = 'product_line_NDS'
+
+update DATA_FLOW SET LSET = '2006-01-01 00:00:00' WHERE NAME = 'city_NDS'
+update DATA_FLOW SET CET = '2006-01-01 00:00:00' WHERE NAME = 'city_NDS'
+
 --Thêm giá trị của bảng stage vào dataflow để quản lý
 INSERT INTO DATA_FLOW (NAME, STATUS, LSET, CET)
 VALUES ('supermarket_sales_STAGE', 1, '2006-01-01 00:00:00', '2007-01-01 00:00:00'),
 ('product_STAGE', 1, '2006-01-01 00:00:00', '2007-01-01 00:00:00'),
 ('product_line_STAGE', 1, '2006-01-01 00:00:00', '2007-01-01 00:00:00'),
-('city_STAGE', 1, '2006-01-01 00:00:00', '2007-01-01 00:00:00')
+('city_STAGE', 1, '2006-01-01 00:00:00', '2007-01-01 00:00:00'),
+
+('supermarket_sales_NDS', 1, '2006-01-01 00:00:00', '2007-01-01 00:00:00'),
+('product_NDS', 1, '2006-01-01 00:00:00', '2007-01-01 00:00:00'),
+('product_line_NDS', 1, '2006-01-01 00:00:00', '2007-01-01 00:00:00'),
+('city_NDS', 1, '2006-01-01 00:00:00', '2007-01-01 00:00:00')
---
+++ InitialDB/2.CreateDatabase_NDS.sql
@@ -0,0 +1,65 @@
+create table supermarket_sales_NDS(
+	id int IDENTITY(1, 1),
+	[Invoice ID] varchar(11),
+	[Branch] varchar(1),
+	[Customer type]	varchar(6),
+	[Gender] varchar(6),
+	[ProductID]	varchar(6),
+	[Quantity] int,
+	[Tax 5%] decimal(10,4),
+	[Total] decimal(10,4),
+	[Date] datetime,
+	[Time] datetime,
+	[Payment] varchar(11),
+	[cogs] decimal(10,2),
+	[gross margin percentage] decimal(10,9),
+	[gross income]	decimal(10,4),
+	[Rating] decimal(10,1),
+	[createdDate]	datetime,
+	[updatedDate]	datetime,
+	
+	CONSTRAINT PK_supermarket_sales_NDS
+	PRIMARY KEY CLUSTERED (id)
+)
+
+create table product_NDS(
+	id int IDENTITY(1, 1),
+	[ProductID] varchar(6),
+	[Unit price] float,
+	[ProductLine] varchar(50),
+	[createdDate] datetime,
+	[updatedDate] datetime,
+	
+	CONSTRAINT PK_product_NDS
+	PRIMARY KEY CLUSTERED (id)
+)
+
+create table product_line_NDS(
+	id int IDENTITY(1, 1),
+	[Product line] varchar(50),
+	[ProductLineID] varchar(5),
+	[createdDate] datetime,
+	[updatedDate] datetime,
+
+	CONSTRAINT PK_product_line_NDS
+	PRIMARY KEY CLUSTERED (id)
+)
+
+create table city_nds(
+	id int IDENTITY(1, 1),
+	[Branch] varchar(1),
+	[City] varchar(50),
+	[createdDate] datetime,
+	[updatedDate] datetime,
+
+	CONSTRAINT PK_city_NDS
+	PRIMARY KEY CLUSTERED (id)
+)
+
+select * from supermarket_sales_NDS;
+
+select * from product_NDS;
+
+select * from product_line_NDS;
+
+select * from city_NDS;

dd69121be22d3714b24e049df80b15f9c9334123
huynhminhduc <105966989+Huynhminhduc2k2@users.noreply.github.com>
2023-12-16 21:36:52 +0700

    Add DDS tables

---
+++ InitialDB/3.CreateDatabase_DDS.sql
@@ -0,0 +1,49 @@
+﻿create database DDS
+use DDS
+go
+
+create table gender_DIM(
+	idGender_SK int NOT NULL PRIMARY KEY,
+	GioiTinh nvarchar(255)
+)
+
+create table khachHang_DIM(
+	idKhachHang_SK nvarchar(255) NOT NULL PRIMARY KEY,
+	idGender int,
+	idCity nvarchar(255),
+	CustomerType nvarchar(255),
+)
+
+create table city_DIM(
+	idCity_SK nvarchar(255) NOT NULL PRIMARY KEY,
+	city nvarchar(255)
+)
+
+create table date_DIM(
+	idDate_SK nvarchar(255) NOT NULL PRIMARY KEY,
+	[day] nvarchar(255),
+	[month] nvarchar(255),
+	[year] nvarchar(255)
+)
+
+create table product_DIM
+(
+	idProduct nvarchar(255) NOT NULL PRIMARY KEY,
+	Quantity float,
+	ProductLine nvarchar (255),
+)
+
+create table payment_DIM
+(
+	idPayment nvarchar(255) PRIMARY KEY ,
+	PaymentMethod nvarchar(255),
+)
+
+create table Fact(
+InvoiceID nvarchar(255)NOT NULL PRIMARY KEY,
+idProduct nvarchar(255)NOT NULL,
+idKhachHang nvarchar(255)NOT NULL,
+idPayment nvarchar(255)NOT NULL,
+Rating float NOT NULL,
+grossIncome float NOT NULL
+)
